c1cd3544b9c2453576ba802e84219ce57d1dc6c8
Pedrotricossi <pedro@ambientelivre.com.br>
2021-08-09 12:55:51 -0300

    Funciona para animes

---
+++ pega_frase.py
@@ -0,0 +1,19 @@
+import json
+import requests
+
+def request():
+    r = requests.get('https://animechan.vercel.app/api/random')
+
+    return r
+
+def anime(anime):
+
+    return anime.json()["anime"]
+
+def personagem(anime):
+
+    return anime.json()["character"]
+
+def frase(anime):
+
+    return anime.json()["quote"]
